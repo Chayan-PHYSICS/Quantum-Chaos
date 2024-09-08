f0726d58789203c1f8deb441914b3952f16175fc
Chayan_PHYSICS <96951199+Chayan-PHYSICS@users.noreply.github.com>
2024-09-08 16:39:32 +0530

    Add files via upload

---
+++ energy_vs_eta.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from qutip import *
+
+# Parameters
+beta = 0.5
+j = 5
+
+Jx = jmat(j, 'x')
+Jy = jmat(j, 'y')
+Jz = jmat(j, 'z')
+
+alpha = np.linspace(0, 15, 1000)
+phases_vs_alpha = np.zeros((len(alpha), (2*j+1)))
+
+# Loop over alpha and compute the Floquet operator and its eigenvalues
+for row, i in enumerate(alpha):
+    F0 = (-1j * beta * Jx).expm()
+    F1 = (-1j * i * (Jz * Jz) / (2 * j)).expm()  # Use .expm() for the correct matrix exponentiation
+    F = F0 * F1
+    F = F.full()
+    eigs = np.linalg.eigvals(F)
+    phases = np.angle(eigs)
+    # Convert phases from [-π, π] to [0, 2π]
+    # phases = (phases + 2 * np.pi) % (2 * np.pi)
+    phases_vs_alpha[row, :] = -phases
+
+
+# Plotting for visualization
+plt.figure(figsize=(10, 6))
+for i in range(phases_vs_alpha.shape[1]):
+    plt.plot(alpha, phases_vs_alpha[:, i], '.',markersize = 1, color = 'k')
+plt.xlabel(r'$\alpha$')
+plt.ylabel('Phase')
+plt.title(r'Phases of Eigenvalues vs $\alpha$')
+# plt.legend()
+plt.show()
+
